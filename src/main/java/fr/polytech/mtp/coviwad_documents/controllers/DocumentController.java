fe732ecbc1189bffe1fd7b998fb50f191338dc5a
Marine Téroitin <55442169+marineteroitin@users.noreply.github.com>
2021-11-09 23:03:21 +0100

    [ADD] cors autorisation

--- src/main/java/fr/polytech/mtp/coviwad_documents/controllers/DocumentController.java
+++ src/main/java/fr/polytech/mtp/coviwad_documents/controllers/DocumentController.java
@@ -14,6 +14,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api/documents")
 public class DocumentController {
